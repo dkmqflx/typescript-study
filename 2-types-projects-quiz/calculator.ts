e1d5023c48811379a5550a83d23c1f975c0d392c
dkmqflx <dkmqflx@gmail.com>
2021-07-15 23:37:27 +0900

    Add contents

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -2,31 +2,28 @@
  * Let's make a calculator 🧮
  */
 
-type Command = 'add'|'substract'|'multiply'|'divide'|'remainder'
+// command를 string  타입으로 지정하지 않는 것은, 임의의 문자열이 아니라 지정된 command가 있기 때문이다
+type Command = "add" | "substract" | "multiply" | "divide" | "remainder";
 
-function calculate(command: Command, num1:number, num2:number):number{
-
-  switch(command){
-    case 'add':
-      return num1+num2
-    case 'substract':
-      return num1-num2
-    case 'multiply':
-      return num1*num2
-    case 'divide':
-      return num1/num2
-    case 'remainder':
-      return num1%num2
+function calculate(command: Command, num1: number, num2: number): number {
+  switch (command) {
+    case "add":
+      return num1 + num2;
+    case "substract":
+      return num1 - num2;
+    case "multiply":
+      return num1 * num2;
+    case "divide":
+      return num1 / num2;
+    case "remainder":
+      return num1 % num2;
     default:
-      throw new Error("unknown command")
+      throw new Error("unknown command");
   }
-
 }
 
-
-
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -3,43 +3,46 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   // ResourceLoadState은 union type
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 
-  function printLoginState(state:ResourceLoadState){
-    switch(state.state){
-      case 'loading':
-        console.log('loading')
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("loading");
         break;
-      case 'success':
-        console.log(state.response.body)
+      case "success":
+        console.log(state.response.body);
         break;
-      case 'fail':
-        console.log(state.reason)
+      case "fail":
+        console.log(state.reason);
         break;
       default:
-        throw new Error(`unknown state ${state}`) 
-
+        throw new Error(`unknown state ${state}`);
     }
-
   }
+
+  // if vs switch ?
+
+  // 대게 유니온 타입경우 여러 케이스별로 다른 로직을 적용할때는 switch가 더 좋아요.
+  // 지금 같은 경우에도 다른 타입(retrying 재시도중)을 추가 한다면 추가된 케이스는 전부다 else에 들어가서 예상하지 못한 오류가 발생하겠죠~? :)
 }
