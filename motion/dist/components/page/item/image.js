e09dd31ccd3c70e8433c360191e003f90f97fda9
dkmqflx <dkmqflx@gmail.com>
2021-08-21 14:35:38 +0900

    14,12 모든 섹션들 구현하기 (노트편)

--- motion/dist/components/page/item/image.js
+++ motion/dist/components/page/item/image.js
@@ -5,7 +5,7 @@ export class ImageComponent extends BaseComponent {
         <div class="image__holder">
           <img class="image__thumbnail" />
         </div>
-        <p class="image__title"></p>
+        <h2 class="image__title"></h2>
       </setion>`);
         const imageElement = this.element.querySelector('.image__thumbnail');
         imageElement.src = url;
